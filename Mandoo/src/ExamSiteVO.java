9a97a2366f8930eab8b5ddc06ce528dabc377392
JeongBeee <dun05139@gmail.com>
2023-04-12 21:34:01 +0900

    ExamSiteVO 수정

--- Mandoo/src/ExamSiteVO.java
+++ Mandoo/src/ExamSiteVO.java
@@ -1,5 +1,49 @@
 
 public class ExamSiteVO {
-	String siteName;
+
+	private String siteCode;
+	private String examTime;
+	private String examDate;
+	private String siteName;
+
+	public ExamSiteVO(String siteCode, String examTime, String examDate, String siteName) {
+		super();
+		this.siteCode = siteCode;
+		this.examTime = examTime;
+		this.examDate = examDate;
+		this.siteName = siteName;
+	}
+
+	public String getSiteCode() {
+		return siteCode;
+	}
+
+	public void setSiteCode(String siteCode) {
+		this.siteCode = siteCode;
+	}
+
+	public String getExamTime() {
+		return examTime;
+	}
+
+	public void setExamTime(String examTime) {
+		this.examTime = examTime;
+	}
+
+	public String getExamDate() {
+		return examDate;
+	}
+
+	public void setExamDate(String examDate) {
+		this.examDate = examDate;
+	}
+
+	public String getSiteName() {
+		return siteName;
+	}
+
+	public void setSiteName(String siteName) {
+		this.siteName = siteName;
+	}
 
 }
--- Mandoo/src/ExamVO.java
+++ Mandoo/src/ExamVO.java
@@ -1,4 +1,38 @@
 
 public class ExamVO {
+	private String examCode;
+	private String examName;
+	private String fee;
+
+	public ExamVO(String examCode, String examName, String fee) {
+		super();
+		this.examCode = examCode;
+		this.examName = examName;
+		this.fee = fee;
+	}
+
+	public String getExamCode() {
+		return examCode;
+	}
+
+	public void setExamCode(String examCode) {
+		this.examCode = examCode;
+	}
+
+	public String getExamName() {
+		return examName;
+	}
+
+	public void setExamName(String examName) {
+		this.examName = examName;
+	}
+
+	public String getFee() {
+		return fee;
+	}
+
+	public void setFee(String fee) {
+		this.fee = fee;
+	}
 
 }
