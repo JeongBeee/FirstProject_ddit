9efadbb655d9d2e105c868d8ead86b1ab9c971ef
leeheyjin <amideveloper@naver.com>
2023-04-12 20:42:39 +0900

    RegisterVO 업데이트

--- Mandoo/src/RegisterVO.java
+++ Mandoo/src/RegisterVO.java
@@ -1,4 +1,38 @@
 
 public class RegisterVO {
+	private String id;
+	private String examCode;
+	private String siteCode;
+
+	public RegisterVO(String id, String examCode, String siteCode) {
+		super();
+		this.id = id;
+		this.examCode = examCode;
+		this.siteCode = siteCode;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getExamCode() {
+		return examCode;
+	}
+
+	public void setExamCode(String examCode) {
+		this.examCode = examCode;
+	}
+
+	public String getSiteCode() {
+		return siteCode;
+	}
+
+	public void setSiteCode(String siteCode) {
+		this.siteCode = siteCode;
+	}
 
 }
