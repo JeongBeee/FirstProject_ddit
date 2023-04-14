67b4b0e9ce7099c5d4ba1f7afc882f52a782931b
JeongBeee <dun05139@gmail.com>
2023-04-14 12:02:07 +0900

    FrontController, RegisterView 수정

--- Mandoo/src/application/FrontController.java
+++ Mandoo/src/application/FrontController.java
@@ -21,11 +21,25 @@ public class FrontController {
 		case 1: // 회원가입
 			ExamineeVO examinees = registerView.insertSignInfo(scanner); // 회원가입 창 메서드를 불러서 
 			int insertExaminee = service.insertMyInfo(examinees);
+			if (insertExaminee > 0) {
+				System.out.println("회원가입이 완료되었습니다. 첫 화면으로 돌아갑니다.");
+			} else {
+				System.out.println("회원가입이 실패하였습니다. 다시 진행해주세요.");
+			}
 			break;
-		//case 2: // 로그인
-		//	ExamineeVO examinees = registerView.login(scanner);
-		//	int selectExaminee = service.selectMyInfo(examinees);
-		//	break;
+		case 2: // 로그인
+			ExamineeVO examinees = registerView.login(scanner);
+			int selectExaminee = service.selectMyInfo(examinees);
+			break;
+		}
+		int modifyinfoMenu = registerView.modifyinfoMenu(scanner);
+		switch(modifyinfoMenu) {
+		case 1: //비밀번호
+			ExamineeVO examinees = registerView.modifySigninfoMenu(scanner);
+			
+		case 2: //전화번호
+		case 3: //이메일
+		case 4: //회원탈퇴			
 		}
     }
 	}
